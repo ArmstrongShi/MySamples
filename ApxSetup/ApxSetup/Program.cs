955f4d655d958073980e8e39267cf4b8ff2e1d6a
Armstrong Shi <ashi@sscinc.com>
2020-03-05 20:42:04 -0800

    refactor

--- ApxSetup/ApxSetup/Program.cs
+++ ApxSetup/ApxSetup/Program.cs
@@ -6,38 +6,25 @@ namespace Advent.ApxSetup
         static void Main(string[] args)
         {
             // When Web Server and App Server are different machines, we must do following
-            Test_HTTP();
-            //Test_HTTPS();
-        }
-
-        static void Test_HTTP()
-        {
+            string protocol = "https";
             string appServerFqdn = "vmapxba9.advent.com";
             string webServerFqdn = "vmapxba9.advent.com";
             string newUIServerFqdn = "vmapxba9.advent.com";
-            // 1. Install Identity Server Self-signed Cert on Web Machine
-            //IdentityServerHelper.InstallIdsAppServerCertificate(appServerFqdn);
-
-            // 2. Register APX Web's redirect urls to Identity Server
-            IdentityServerHelper.RegisterApxWebRedirectUris("http://" + webServerFqdn + "/oauth/", "admin", "advs", webServerFqdn);
-
-            // 3. Register APX New UI's redirect Urls to Identity Server
-            //IdentityServerHelper.RegisterNewUIRedirectUris("http://" + webServerFqdn + "/", "admin", "advs", newUIServerFqdn);
+            Test(protocol, appServerFqdn, webServerFqdn, newUIServerFqdn);
         }
 
-        static void Test_HTTPS()
+        static void Test(string protocol, string appServerFqdn, string webServerFqdn, string newUIServerFqdn)
         {
-            string appServerFqdn = "vmapxba8.advent.com";
-            string webServerFqdn = "vmapxba8.advent.com";
-            string newUIServerFqdn = "vmapxba8.advent.com";
             // 1. Install Identity Server Self-signed Cert on Web Machine
             IdentityServerHelper.InstallIdsAppServerCertificate(appServerFqdn);
 
             // 2. Register APX Web's redirect urls to Identity Server
-            IdentityServerHelper.RegisterApxWebRedirectUris("https://" + webServerFqdn + "/oauth/", "admin", "advs", webServerFqdn);
+            string idsProxyUrl = protocol + "://" + webServerFqdn + "/oauth/";
+            IdentityServerHelper.RegisterApxWebRedirectUris(idsProxyUrl, "admin", "advs", webServerFqdn);
 
             // 3. Register APX New UI's redirect Urls to Identity Server
-            IdentityServerHelper.RegisterNewUIRedirectUris("https://" + webServerFqdn + "/", "admin", "advs", newUIServerFqdn);
+            string apiServerUrl = protocol + "://" + webServerFqdn + "/";
+            IdentityServerHelper.RegisterNewUIRedirectUris(apiServerUrl, "admin", "advs", newUIServerFqdn);
         }
     }
 }
