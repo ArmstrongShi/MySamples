678be594dd37d9c6362df623c83bdb5bcc4eab9d
Armstrong Shi <ashi@sscinc.com>
2020-04-13 20:17:20 -0700

    powershell

--- Poweshell/EnableWindowsFeatures.ps1
+++
@@ -1,5 +0,0 @@
-
-install-windowsfeature -name NET-Framework-Features -IncludeAllSubFeature
-install-windowsfeature -name NET-Framework-45-Features -IncludeAllSubFeature
-install-windowsfeature -name MSMQ -IncludeAllSubFeature
-install-windowsfeature -name Web-WebServer -IncludeAllSubFeature -IncludeManagementTools
